991c58e4126ad9073bcade460955c8ae790ac0bf
Galvanizer6 <juliang478@gmail.com>
2017-01-24 20:37:55 -0800

    Added & Refactored

--- src/main/java/com/galvanizer6/dasbier/DasBierMain.java
+++ src/main/java/com/galvanizer6/dasbier/DasBierMain.java
@@ -1,7 +1,13 @@
 package com.galvanizer6.dasbier;
 
+import org.apache.logging.log4j.Logger;
+
+import com.galvanizer6.dasbier.proxy.CommonProxy;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -9,20 +15,27 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = References.MOD_ID, name=References.MOD_NAME, version = References.VERSION, acceptedMinecraftVersions= "[1.11, 1.11.2]")
 public class DasBierMain
 {   
-    
-	@EventHandler
-	public static void preInit(FMLPreInitializationEvent event) {
-		
-	}
+	@SidedProxy(clientSide = References.CLIENT_SIDE, serverSide = References.SERVER_SIDE)
+    public static CommonProxy proxy;
 	
+	@Instance(References.MOD_ID)
+	public static DasBierMain instance;
+	
+	public static Logger logger;
+	
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		proxy.preInit(event);
+	}	
 	
     @EventHandler
     public void init(FMLInitializationEvent event) {
-      
+      proxy.init(event);
     }
     
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-    	
+    	proxy.postInit(event);
     }
 }
